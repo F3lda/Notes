e671a26fba583c616e179310d80f35241a5080a3
= <kajel.jirgl@gmail.com>
2025-11-15 16:26:10 +0100

    Note class updated for Hive

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,9 +1,19 @@
 
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'note.g.dart';
+
+@HiveType(typeId: 0)
 class Note {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String content;
+  @HiveField(3)
   final DateTime createdAt;
+  @HiveField(4)
   DateTime updatedAt;
 
   Note({
